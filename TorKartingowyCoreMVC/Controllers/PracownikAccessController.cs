32646a82dc7cdbf83249f732284301d1be46bb97
Kuba <kubson908@wp.pl>
2023-01-25 18:02:32 +0100

    Zmiana hasła dla pracownika i wyszukiwanie w magazynie

--- TorKartingowyCoreMVC/Controllers/PracownikAccessController.cs
+++ TorKartingowyCoreMVC/Controllers/PracownikAccessController.cs
@@ -6,6 +6,7 @@ using TorKartingowyCoreMVC.Models;
 using TorKartingowyCoreMVC.Data;
 using System.Text;
 using System.Security.Cryptography;
+using Microsoft.EntityFrameworkCore;
 
 namespace TorKartingowyCoreMVC.Controllers
 {
@@ -103,6 +104,43 @@ namespace TorKartingowyCoreMVC.Controllers
                 return View(pracownik);
             }
             return RedirectToAction("Index", "Home");
+        }
+
+        //GET
+        public IActionResult ChangePassword(int? id)
+        {
+            ClaimsPrincipal claimUser = HttpContext.User;
+
+            if (claimUser.Identity == null ||
+                !claimUser.Identity.IsAuthenticated ||
+                User.Claims.FirstOrDefault(c => c.Type == "Role").Value == "Klient") return RedirectToAction("Index", "Home");
+
+            var dane = _db.Pracownicy.Find(id);
+            return View(dane);
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult ChangePassword(Pracownik obj, string pass, string pass1, string pass2)
+        {
+            if (ModelState.IsValid)
+            {
+                bool confirm = pass1.Equals(pass2);
+                if (hashPassword(pass).Equals(obj.Haslo) && confirm
+                    && hashPassword(pass1) != obj.Haslo && pass1.Length >= 6)
+                {
+                    _db.Database.ExecuteSqlRaw("UPDATE Pracownicy SET Haslo = '" + hashPassword(pass1) + "' WHERE Id = '" + obj.Id + "';");
+                    TempData["success"] = "Zaktualizowano hasło";
+                    return View("WyswietlKonto", obj);
+                }
+                else if (!hashPassword(pass).Equals(obj.Haslo)) TempData["error"] = "Błędne hasło";
+                else if (!confirm) TempData["error"] = "Hasła nie są takie same";
+                else if (hashPassword(pass1) == obj.Haslo) TempData["error"] = "Nowe hasło nie może być takie jak stare";
+                else if (pass1.Length < 6) TempData["error"] = "Hasło jest za krótkie";
+
+                return View(obj);
+            }
+            return View(obj);
         }
     }
 }
